c5d5cb99226e52d9aaa9b131470b8087490d5fb9
Thompson Edolo <verygreenboi@live.com>
2016-05-10 00:10:26 +0100

    Add build dependant constants classes

---
+++ mobile/src/debug/java/net/glassstones/bambammusic/Constants.java
@@ -0,0 +1,29 @@
+package net.glassstones.bambammusic;
+
+public class Constants {
+    public static final String PARSE_APP_KEY = "PwYxMbm5w2ztP5EKIx1bqt5HZIYMoHFy3b7xhDKF";
+    public static final String PARSE_CLIENT_KEY = "vdt6fZ35u9spxuoDdpHzCm9bvqRNCufKarlzhzbk";
+    public static final String PARSE_REST_KEY = "lQd0hFfxV58CadtKoEgl5D2kAAKa34AH8lgjJEP1";
+
+    public static final String KEY_TUNELINE_URL = "http://10.0.2.2:1337/parse/functions/tuneline";
+    public static final String KEY_TUNELINE_STATUS = "isTunelineFresh";
+
+    public static final String KEY_TUNELINE_ARRAY_NAME = "result";
+    public static final String TYPE_IMAGE = "image";
+    public static final String TYPE_VIDEO = "video";
+    public static final String TYPE_VOICE_NOTE = "voiceNote";
+
+    public static final String TYPE_MP3 = "mp3";
+
+    public static final String PUBLISHABLE_KEY = "pk_test_e6b1b4905246144ffbca097408db55ff9e982616";
+    public static final String SERVER_URL = "http://10.0.2.2:1337/parse/"; //For dev
+    public static final int TUNE_SAVE_STATUS_OK = 0;
+    public static final int TUNE_GET_FAILURE = -11;
+    public static final int TUNE_ACTIVITY_SAVE_FAILURE = -2;
+
+    public static final int SAVE_COMMENT_FAILURE = -3;
+    public static final int COMMENT_STATUS_CREATED = 0;
+    public static final int COMMENT_STATUS_UPLOADED = 1;
+
+    public static final int COMMENT_STATUS_UPLOAD_FAILED = -111;
+}
